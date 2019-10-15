2ae9801b1ec1d0e4163c7beee58500e9f1d87237
AAGODOY <aldanaabril.godoy@gmail.com>
2019-10-14 22:43:34 -0300

    Clase estática (inicio) config de usuario

---
+++ src/BE/ConfigUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public static class ConfigUsuario
+    {
+        public static List<Patente> patentes { get; set; }
+        public static List<Familia> familias { get; set; }
+        public static string idioma { get; set; }
+    }
+}
