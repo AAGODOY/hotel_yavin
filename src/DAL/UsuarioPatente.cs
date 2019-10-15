fd66c8670c3fac95b29464bf134d4ee51089067f
AAGODOY <aldanaabril.godoy@gmail.com>
2019-10-15 00:26:18 -0300

    Nueva query para traer patentes por id usuario y detalle de patentes

--- src/DAL/UsuarioPatente.cs
+++ src/DAL/UsuarioPatente.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DAL
 {
@@ -30,6 +32,59 @@ namespace DAL
         public List<BE.UsuarioPatente> SelectAll()
         {
             throw new NotImplementedException();
+        }
+
+        public List<BE.UsuarioPatente> SelectById(int id)
+        {
+            string query = $"Select * From UsuarioPatente where Id = {id}";
+            using (SqlDataReader dataReader = helper.ExecuteReader(query))
+            {
+                List<BE.UsuarioPatente> usuPat = new List<BE.UsuarioPatente>();
+                while (dataReader.Read())
+                {
+                    BE.UsuarioPatente usuarioPatentes = MapDataReaderUsuPat(dataReader);
+                    usuPat.Add(usuarioPatentes);
+                }
+
+                return usuPat;
+            }
+        }
+
+        public List<BE.Patente> GetPatentes(int id_usuario)
+        {
+            string query = $"SELECT Patente.id_patente, Patente.descripcion, Patente.activo FROM UsuarioPatente INNER JOIN Patente ON UsuarioPatente.id_patente = Patente.id_patente WHERE id_usuario = {id_usuario}";
+            using (SqlDataReader dataReader = helper.ExecuteReader(query))
+            {
+                List<BE.Patente> patente_list = new List<BE.Patente>();
+                while (dataReader.Read())
+                {
+                    BE.Patente usuarioPatentes = MapDataReaderPat(dataReader);
+                    patente_list.Add(usuarioPatentes);
+                }
+
+                return patente_list;
+            }
+
+        }
+
+        private BE.UsuarioPatente MapDataReaderUsuPat(SqlDataReader dataReader) {
+            BE.UsuarioPatente usuPat = new BE.UsuarioPatente();
+            usuPat.id_patente = dataReader.GetInt32(0);
+            usuPat.id_usuario = dataReader.GetInt32(1);
+            usuPat.DVH = dataReader.GetInt32(2);
+            usuPat.patenteNegada = dataReader.GetBoolean(3);
+
+            return usuPat;
+        }
+
+        private BE.Patente MapDataReaderPat(SqlDataReader dataReader)
+        {
+            BE.Patente patente = new BE.Patente();
+            patente.id = dataReader.GetInt32(0);
+            patente.descripcion = dataReader.GetString(1);
+            patente.activo = dataReader.GetBoolean(2);
+
+            return patente;
         }
     }
 }
